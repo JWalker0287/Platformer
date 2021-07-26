3682f1471559dec569d8623ba805c4a3fedaa9f8
Nolan Baker <nolan.lee.baker@gmail.com>
2021-07-26 13:30:00 -0500

    adds basic player movement and jumping

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 2;
+    public float jumpHeight = 3;
+    public LayerMask envLayer;
+
+    bool onGround;
+    Rigidbody2D body;
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        GroundCheck();
+        float x = Input.GetAxis("Horizontal");
+        if (x != 0) transform.right = Vector2.right * x;
+
+        body.velocity = new Vector2(x * speed, body.velocity.y);
+
+        if (onGround && Input.GetButtonDown("Jump"))
+        {
+            float jumpVelocity = Mathf.Sqrt(-2 * Physics2D.gravity.y * body.gravityScale * jumpHeight);
+            body.velocity = new Vector2(body.velocity.x, jumpVelocity);
+        }
+    }
+
+    void GroundCheck ()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.05f, envLayer);
+        onGround = (colliders.Length > 0);
+    }
+}
