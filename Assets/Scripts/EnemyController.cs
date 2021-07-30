2a418a858ae50a813d6999a72fd17f14dad71a1c
Jonathan Walker <jwsmasher@gmail.com>
2021-07-30 10:20:13 -0500

    Added basic enemy script that has enemies pace back and fourth; Enemies do damage when they touch the player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    void Start()
+    Rigidbody2D body;
+    public float turnAroundTime = 0;
+    public float turnAroundInterval = 3;
+    public float speed = 2;
+    Vector2 leftRight;
+    bool goingRight;
+
+    void Awake()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+        leftRight = Vector2.right;
+        goingRight = true;
     }
 
     void Update()
     {
-        
+        body.velocity = new Vector2(speed * leftRight.x, body.velocity.y);
+
+        if (turnAroundTime > turnAroundInterval)
+        {
+            if(goingRight)
+            {
+                leftRight = Vector2.left;
+                transform.right = Vector2.left;
+                goingRight = false;
+            }
+            else
+            {
+                leftRight = Vector2.right;
+                transform.right = Vector2.right;
+                goingRight = true;
+            }
+            turnAroundTime = 0;
+        }
+    turnAroundTime += Time.deltaTime;
     }
 }
