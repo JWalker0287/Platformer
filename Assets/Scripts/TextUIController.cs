c319450d21c65824407a243995a6f879f2b5f8f0
TheGreatRMA <ryan.ansus@yahoo.com>
2021-08-03 11:07:51 -0500

    Add sign that uses a button

---
+++ Assets/Scripts/TextUIController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextUIController : MonoBehaviour
+{
+    public string name;
+    public GameObject inputfield;
+    public GameObject textDisplay;
+
+    public void StoreName()
+    {
+        name = inputfield.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = "Hello knight" + name + "Welcome";
+    }
+}
