4f4db8acd6cda43eb2f0804eeddb5ff2c7185088
Nolan Baker <nolan.lee.baker@gmail.com>
2021-08-31 11:11:58 -0500

    BushBoi done

--- Assets/Scripts/PaceBackAndForthController.cs
+++ Assets/Scripts/PaceBackAndForthController.cs
@@ -12,6 +12,30 @@ public class PaceBackAndForthController : MonoBehaviour
     
     void Update()
     {
-        motor.Move(transform.right.x * motor.naturalDirection);    
+        motor.Move(transform.right.x * motor.naturalDirection);
+
+        Collider2D c = Physics2D.OverlapPoint(
+            transform.position + 
+            transform.right * motor.naturalDirection * 0.5f - 
+            new Vector3(0, 0.1f, 0)
+        );
+        if (c == null) TurnAround(); 
+    }
+
+    void OnCollisionEnter2D (Collision2D c) 
+    {
+        foreach (ContactPoint2D contact in c.contacts)
+        {
+            if (Vector2.Dot(transform.right * motor.naturalDirection, contact.normal) < -0.707f)
+            {
+                TurnAround();
+            }
+        }
+    }
+
+    void TurnAround ()
+    {
+        transform.right = -transform.right;
+        motor.Move(transform.right.x * motor.naturalDirection);
     }
 }
