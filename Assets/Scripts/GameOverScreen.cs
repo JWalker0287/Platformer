466fd0faeb80515361d9d1cf8a6d21e22c30dcd5
TheGreatRMA <ryan.ansus@yahoo.com>
2021-09-08 09:14:20 -0500

    updated restart screen

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -11,7 +11,7 @@ public class GameOverScreen : MonoBehaviour
     public void Setup(int score)
     {
         gameObject.SetActive(true);
-        pointsText.text = score.ToString() + " POINTS";
+        // pointsText.text = score.ToString() + " POINTS";
     }
 
     public void RestartButton()
